d83124a0942abe5c1c0e3cc9b9afeb534ed8934c
Salman <salman@gmail.com>
2026-02-19 09:26:25 +0700

    up file main bag-2

--- src/main.py
+++ src/main.py
@@ -1,3 +1,24 @@
 import Database
 import Data-Buku
 import Peminjaman
+
+def menu_utama():
+    print("\n========== SISTEM PERPUSTAKAAN ==========")
+    print("1. Pendataan Buku")
+    print("2. Peminjaman Buku")
+    print("3. Keluar")
+
+
+def menu_data():
+    print("\n------ MENU DATA BUKU ------")
+    print("1. Tambah Buku")
+    print("2. Lihat Buku")
+    print("3. Hapus Buku")
+    print("4. Kembali")
+
+
+def menu_peminjaman():
+    print("\n------ MENU PEMINJAMAN ------")
+    print("1. Pinjam Buku")
+    print("2. Kembalikan Buku")
+    print("3. Kembali")
